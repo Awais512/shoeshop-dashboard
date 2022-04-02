53c81f3f79bf45c24c704d386eb0b81974bb780d
awais512 <raza.awais25@gmail.com>
2022-04-02 16:54:39 +0500

    Login

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -5,9 +5,15 @@ import {
   USER_LOGOUT,
 } from '../Constants/UserContants';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
-// LOGIN
 export const login = (email, password) => async (dispatch) => {
+  const ToastObjects = {
+    pauseOnFocusLoss: false,
+    draggable: false,
+    pauseOnHover: false,
+    autoClose: 2000,
+  };
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
 
@@ -22,16 +28,28 @@ export const login = (email, password) => async (dispatch) => {
       { email, password },
       config
     );
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    if (!data.isAdmin === true) {
+      toast.error('You are not Admin', ToastObjects);
+      dispatch({
+        type: USER_LOGIN_FAIL,
+      });
+    } else {
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    }
 
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout());
+    }
     dispatch({
       type: USER_LOGIN_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
+      payload: message,
     });
   }
 };
